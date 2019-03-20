ec46906fb15b47cc6eeac9fb771e2c6a6db9aa1e
eric <howcrazys@gmail.com>
2019-03-20 09:21:41 +0800

    docs 設定

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    publicPath: './',
+    outputDir: 'docs',
+    assetsDir: '../docs',
+    runtimeCompiler: undefined,
+    productionSourceMap: undefined,
+    parallel: undefined,
+    css: undefined
+}
