4a7e8d476101f3b84ed47edf89b1e80991c2e5e8
eric <howcrazys@gmail.com>
2019-03-28 14:22:39 +0800

    中間內容完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -49,6 +49,14 @@
     </div>
     <!-- 內容 -->
     <section class="content mt-md-9 mb-md-10">
+      <div class="row no-gutters justify-content-center d-md-none my-5">
+        <div class="col-6">
+          <div>
+            <img class="img-fluid" src="../assets/images/sm-橫式-為什麼選擇了做甜點.svg" alt="為什麼選擇了做甜點">
+          </div>
+        </div>
+      </div>
+
       <div class="bg-primary-lighter">
         <div class="container">
           <div class="row">
@@ -71,6 +79,57 @@
               </div>
             </div>
           </div>
+        </div>
+      </div>
+    </section>
+    <!-- 為什麼一定要吃甜點 -->
+    <section class="content mt-md-9 mb-md-10">
+      <div class="row no-gutters justify-content-center d-md-none my-5">
+        <div class="col-6">
+          <div>
+            <img class="img-fluid" src="../assets/images/sm-橫式-為什麼一定要吃甜點.svg" alt="為什麼一定要吃甜點">
+          </div>
+        </div>
+      </div>
+
+      <div class="bg-primary-lighter">
+        <div class="container">
+          <!-- 反轉排序 -->
+          <div class="row flex-row-reverse">
+            <div class="col-md-6 minus-md-margin">
+              <div
+                class="bg-cover h-100 content__img__intro"
+                style="background-image:url(https://bit.ly/2zzAQS4)"
+              ></div>
+            </div>
+            <div class="col-md-6 d-flex justify-content-end content__vertical minus-md-margin">
+              <div
+                class="py-md-8 p-5 writing-md-vertical d-flex-reverse text-primary-light justify-content-end mr-auto"
+              >
+                <p class="mb-0">青山依舊在，幾度夕陽紅。慣看秋月春風。</p>
+                <p class="mb-0">一壺濁酒喜相逢，浪花淘盡英雄。是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。</p>
+                <p class="mb-0">是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。</p>
+              </div>
+              <div class="d-none d-md-block">
+                <img src="../assets/images/lg-為什麼一定要吃甜點.svg" width="88" alt="為什麼一定要吃甜點">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="content mt-md-9 mb-md-10">
+      <div class="row no-gutters justify-content-center d-md-none my-5">
+        <div class="col-6">
+          <div>
+            <img class="img-fluid" src="../assets/images/sm-橫式-想吃甜點是不需要理由的.svg" alt="想吃甜點是不需要理由的">
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex justify-content-center">
+        <div class="d-none d-md-block">
+          <img src="../assets/images/lg-想吃甜點是不需要理由的.svg" width="88" alt="想吃甜點是不需要理由的">
         </div>
       </div>
     </section>
