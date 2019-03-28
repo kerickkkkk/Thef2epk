e9a7e46e6c1f6936ed7958ec4971eceb5b5b03a3
eric <howcrazys@gmail.com>
2019-03-28 11:40:48 +0800

    layout完成 首頁ing docs 測試

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,31 +9,71 @@
           ></div>
         </div>
       </div>
+      <!-- 中間三塊區域 -->
       <div class="row justify-content-center threepoint">
-        <div class="col-md-10">
-          <div class="row">
+        <div class="col-md-10 px-0 px-md-3">
+          <div class="row no-gutters">
             <div class="col-4">
-              <a href="#">
+              <a class="threepoint__wrap bg-cover active" href="#">
                 <div class="threepoint__text">本日精選</div>
-                <div class="threepoint__bg"></div>
+                <div class="threepoint__cover"></div>
+                <div
+                  class="threepoint__img bg-cover"
+                  style="background-image: url(https://images.unsplash.com/photo-1525059337994-6f2a1311b4d4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7ce5bbf66bdb4ed3b6965f0b3618aa43&auto=format&fit=crop&w=654&q=80)"
+                ></div>
               </a>
             </div>
             <div class="col-4">
-              <a href="#">
+              <a class="threepoint__wrap bg-cover" href="#">
                 <div class="threepoint__text">人氣推薦</div>
-                <div class="threepoint__bg"></div>
+                <div class="threepoint__cover"></div>
+                <div
+                  class="threepoint__img bg-cover"
+                  style="background-image: url(https://images.unsplash.com/photo-1490914327627-9fe8d52f4d90?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=73ed228f25bdefd2291c242f6d390fca&auto=format&fit=crop&w=634&q=80)"
+                ></div>
               </a>
             </div>
             <div class="col-4">
-              <a href="#">
+              <a class="threepoint__wrap bg-cover" href="#">
                 <div class="threepoint__text">新品上市</div>
-                <div class="threepoint__bg"></div>
+                <div class="threepoint__cover"></div>
+                <div
+                  class="threepoint__img bg-cover"
+                  style="background-image: url(https://images.unsplash.com/photo-1504473089979-b1c4993a9653?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9db69b72e1f7d1b4e5c81cee531108ff&auto=format&fit=crop&w=634&q=80);"
+                ></div>
               </a>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <!-- 內容 -->
+    <section class="content mt-md-9 mb-md-10">
+      <div class="bg-primary-lighter">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 minus-md-margin">
+              <div
+                class="bg-cover h-100 content__img__intro"
+                style="background-image:url(https://images.unsplash.com/photo-1519730722595-a5ff788dea4d?ixlib=rb-0.3.5…jEyMDd9&s=2712a33…&auto=format&fit=crop&w=634&q=80)"
+              ></div>
+            </div>
+            <div class="col-md-6 d-flex justify-content-end content__vertical minus-md-margin">
+              <div
+                class="py-md-8 p-5 writing-md-vertical d-flex-reverse text-primary-light justify-content-end mr-auto"
+              >
+                <p class="mb-0">青山依舊在，幾度夕陽紅。慣看秋月春風。</p>
+                <p class="mb-0">一壺濁酒喜相逢，浪花淘盡英雄。是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。</p>
+                <p class="mb-0">是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。</p>
+              </div>
+              <div class="d-none d-md-block">
+                <img src="../assets/images/lg-為什麼選擇了做甜點.svg" width="88" alt="為什麼選擇了做甜點">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
-// module.exports = {
-//     publicPath: './',
-//     outputDir: 'docs',
-//     assetsDir: '../docs',
-//     runtimeCompiler: undefined,
-//     productionSourceMap: undefined,
-//     parallel: undefined,
-//     css: undefined
-// }
+module.exports = {
+  publicPath: '/docs/',
+  outputDir: 'docs',
+  assetsDir: './'
+}
